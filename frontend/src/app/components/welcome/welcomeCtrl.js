e0b4d5ad204f71d24ebfab811ec59b4eb78dc071
hverlin <hugues.verlin@gmail.com>
2015-10-23 08:20:49 +0200

    Update de la config pour l'api

--- frontend/src/app/components/welcome/welcomeCtrl.js
+++ frontend/src/app/components/welcome/welcomeCtrl.js
@@ -1,8 +1,11 @@
 gfaimApp.controller('welcomeCtrl', ['$scope', 'searchService', '$filter', '$log', '$state',
     function ($scope, searchService, $filter, $log, $state) {
 
+        $scope.isSearching = false;
+
         $scope.search = function () {
-            $state.go("search.results")
+            //$state.go("search.results")
+            $scope.isSearching = true;
         };
 
     }]);
