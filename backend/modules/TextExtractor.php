508e2389114becf2307174e1cd7b45a65c689404
Antoine Chabert <chabert.antoine@gmail.com>
2015-11-03 11:01:17 +0100

    Correction sur TextExtractor

--- backend/modules/TextExtractor.php
+++ backend/modules/TextExtractor.php
@@ -3,20 +3,30 @@
 class TextExtractor
 {
 
+    private static $include_header_code = array(
+        '200'
+    );
+
     /**
-    * Get the text from every URL contained in UrlList
-    * @param $urlList  array containing all the URL
-    * @return Associative array Key = URL and Value = text from the URL
-    */
+     * Get the text from every URL contained in UrlList
+     * @param $urlList  array containing all the URL
+     * @return Associative array Key = URL and Value = text from the URL
+     */
     public static function getAllText($urlList)
     {
         $urlTextList = array();
         foreach($urlList as $url)
         {
             $clearUrl = stripslashes($url);
-            if (!isset($urlTextList[$clearUrl])) {
-                $html = TextExtractor::getHtmlOfURL($clearUrl);
-                $text = TextExtractor::getTextFromHtml($html);
+            $url_headers = @get_headers($clearUrl);
+            $code_header = explode(' ', $url_headers[0])[1];
+
+            if(in_array($code_header, self::$include_header_code) && !isset($urlTextList[$clearUrl]))
+            {
+                $html = self::getHtmlOfURL($clearUrl);
+                $text = '';
+                if($html != null)
+                    $text = self::getTextFromHtml($html);
                 $urlTextList[$clearUrl] = $text;
             }
         }
@@ -24,13 +34,15 @@ class TextExtractor
     }
 
     /**
-    * Get the text from one single URL
-    * @param $url just one URL
-    * @return text from the URL
-    */
+     * Get the text from one single URL
+     * @param $url just one URL
+     * @return text from the URL
+     */
     public static function getHtmlOfURL($url)
     {
         $textFromURL = file_get_contents($url);
+        if($textFromURL == false)
+            return null;
         return $textFromURL;
     }
 
@@ -43,14 +55,14 @@ class TextExtractor
     {
         $dom = new DOMDocument();
         @$dom->loadHTML($html);
-        
+
         $text = '';
         $xpath = new DOMXPath($dom);
         $nodeList = $xpath->query('//div/text()|//h1/text()|//h2/text()|//h3/text()|//span/text()|//p/text()');
-        
+
         $order   = array("\r\n", "\n", "\r");
         $replace = ' ';
-        
+
         foreach($nodeList as $node)
         {
             if(!empty(trim($node->textContent)))
@@ -58,7 +70,7 @@ class TextExtractor
         }
         return $text; // text
     }
-    
+
     /*
     // Ebauche de parseur html plus evolue que strip tags
     
