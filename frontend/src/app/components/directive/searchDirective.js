e33b0718572a0af78b9b8e92b9d82364b5d6f1d5
hverlin <hugues.verlin@gmail.com>
2015-11-05 19:34:28 +0100

    push

--- frontend/src/app/components/directive/searchDirective.js
+++ frontend/src/app/components/directive/searchDirective.js
@@ -14,3 +14,20 @@ gfaimApp.directive('searchResult', function () {
         controller: function ($scope) {}
     };
 });
+
+gfaimApp.directive('actualSrc', function () {
+    return{
+        link: function postLink(scope, element, attrs) {
+            attrs.$observe('actualSrc', function(newVal, oldVal){
+                if(newVal != undefined){
+                    var img = new Image();
+                    img.src = attrs.actualSrc;
+                    angular.element(img).bind('load', function () {
+                        element.attr("src", attrs.actualSrc);
+                    });
+                }
+            });
+
+        }
+    }
+});
